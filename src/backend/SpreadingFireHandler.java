a75ba844d6596fc0cacc6bf660c1c21ddf394ffd
Xingyu Chen <xingyu.chen@duke.edu>
2017-02-09 16:55:30 -0500

    Add Discussion

--- src/backend/SpreadingFireHandler.java
+++ src/backend/SpreadingFireHandler.java
@@ -9,6 +9,9 @@ import java.util.function.Predicate;
  *
  */
 public class SpreadingFireHandler extends Handler {
+	private static final String EMPTY_CELL = "EmptyCell";
+	private static final String TREE = "Tree";
+	private static final String FIRE = "Fire";
 	private double fireProb=0.8;
 	private double treeProb=0.2;
 	
@@ -24,46 +27,44 @@ public class SpreadingFireHandler extends Handler {
 	@Override
 	public void solve(Container curContainer) {
 		// TODO Auto-generated method stub
-		if (curContainer.getMyCell().is("Fire")) {
-			curContainer.getNext().setCell(new EmptyCell());
-			curContainer.getNext().setLocked(true);			
+		if (curContainer.getMyCell().is(FIRE)) {
+			curContainer.setNext(new EmptyCell());
 		}
-		if (curContainer.getMyCell().is("Tree")) {
-			ArrayList<Container> myNeighbor = curContainer.getMyNeighbors();
-			Predicate<String> function = s -> s.compareTo("Fire") == 0;
-			int fireCnt = this.numberLiveNeighbor(myNeighbor, function);
-			boolean fired=false;
-			if (fireCnt>0) {
-				for (int i = 0; i < myNeighbor.size(); i++) {
-					Container curNeighbor=myNeighbor.get(i);
-					if (curNeighbor.getMyCell().is("Fire")) {
-						boolean fireHappened = (Math.random() * 1.0)<fireProb;
-						if (fireHappened) {
-							curContainer.getNext().setCell(new Fire());
-							curContainer.getNext().setLocked(true);
-							fired=true;
-							break;
-						} 
-					}
-				}
-			}
-			
-			if (!fired) {
-				curContainer.getNext().setCell(curContainer.getMyCell());
-				curContainer.getNext().setLocked(true);
-			}
- 		}
 		
-		if (curContainer.getMyCell().is("EmptyCell")) {
+		if (curContainer.getMyCell().is(TREE)) {
+			solveForTree(curContainer);
+		}
+		
+		if (curContainer.getMyCell().is(EMPTY_CELL)) {
 			boolean treeProduced = (Math.random() * 1.0)<treeProb;
 			if (treeProduced) {
-				curContainer.getNext().setCell(new Tree());
-				curContainer.getNext().setLocked(true);
+				curContainer.setNext(new Tree());
 			} else {
-				curContainer.getNext().setCell(new EmptyCell());
-				curContainer.getNext().setLocked(true);
+				curContainer.setNext(new EmptyCell());
 			}
 		}
 	}
-
+	
+	private void solveForTree(Container curContainer) {
+		ArrayList<Container> myNeighbor = curContainer.getMyNeighbors();
+		Predicate<String> function = s -> s.compareTo(FIRE) == 0;
+		int fireCnt = this.numberLiveNeighbor(myNeighbor, function);
+		boolean fired=false;
+		if (fireCnt>0) {
+			for (Container curNeighbor:myNeighbor) {
+				if (curNeighbor.getMyCell().is(FIRE)) {
+					boolean fireHappened = (Math.random() * 1.0)<fireProb;
+					if (fireHappened) {
+						curContainer.setNext(new Fire());
+						fired=true;
+						break;
+					} 
+				}
+			}
+		}
+		
+		if (!fired) {
+			curContainer.setNext(curContainer.getMyCell());
+		}
+	}
 }
